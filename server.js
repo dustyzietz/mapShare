8d23579eeb9469c455f3c87211728c35001226b5
dustyzietz <dustyzietz@gmail.com>
2020-03-24 10:43:38 -1000

    ready to launch

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Map = require('./models/map');
 const app = express();
+const path = require('path');
 
 
 const connectDB = async () => {
@@ -38,9 +39,17 @@ connectDB();
 
 app.use(express.json({ extended: false}));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 app.use('/map', require('./routes/map'));
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (rep,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
