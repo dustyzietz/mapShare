ad040ca7a1cc7a0e612e0a3aa28377daa2be5f66
dustyzietz <dustyzietz@gmail.com>
2020-05-03 14:29:17 -1000

    player page

--- client/src/components/PlayerPage.js
+++ client/src/components/PlayerPage.js
@@ -1,21 +1,31 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 
-export  const PlayerPage = ({ openOne, setOpenOne }) => {
+export  const PlayerPage = ({ openOne, setOpenOne, currentPlayer }) => {
  
- const handleClickOpen = () => {
-    setOpenOne(true);
-  };
-
   const handleClose = () => {
     setOpenOne(false);
   };
 return (   
-      <Dialog maxWidth='sm' fullWidth open={openOne} onClose={handleClose} aria-labelledby="form-dialog-title" style={{zIndex:'1'}}>
-        <DialogContent style={{textAlign: 'center'}}>
-          Thi is the dialog
-  </DialogContent>
+      <Dialog maxWidth='md' fullWidth open={openOne} onClose={handleClose} aria-labelledby="form-dialog-title" style={{zIndex:'1'}}>
+        
+       { currentPlayer &&
+       <div >
+ <h2 style={{textAlign:'center'}}>  {currentPlayer.name} </h2>
+ {console.log(currentPlayer)}
+<img alt="player image" src={currentPlayer.url} width='50%' style={{float:'left', margin:'20px'}}/>
+HP:{currentPlayer.hp}<br/>
+AC:{currentPlayer.ac}<br/>
+Speed:{currentPlayer.speed}<br/>
+Attacks:{currentPlayer.attacks}<br/>
+Spells/Abilities:{currentPlayer.spells}<br/>
+Skills:{currentPlayer.skills}<br/>
+Items:{currentPlayer.Items}<br/>
+
+
+ </div>
+       } 
       </Dialog>
   )
 }
--- client/src/components/SavedPlayers.js
+++ client/src/components/SavedPlayers.js
@@ -1,13 +1,15 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
-import {AddPlayerDialog} from './AddPlayerDialog';
+import { AddPlayerDialog } from './AddPlayerDialog';
 import { playerPage, PlayerPage } from './PlayerPage';
- 
 
-export  const SavedPlayers = ({addPlayer, savedPlayers, getSavedPlayers, addSavedPlayer, deleteSavedPlayer}) => {
+
+export const SavedPlayers = ({ addPlayer, savedPlayers, getSavedPlayers, addSavedPlayer, deleteSavedPlayer }) => {
   const [open, setOpen] = useState(false);
   const [openOne, setOpenOne] = useState(false);
+  const [currentPlayer, setCurrentPlayer] = useState();
+
   const handleClickOpen = () => {
     getSavedPlayers();
     setOpen(true);
@@ -17,38 +19,44 @@ export  const SavedPlayers = ({addPlayer, savedPlayers, getSavedPlayers, addSave
     setOpen(false);
   };
 
+  const openPlayerPage = (p) => {
+    setCurrentPlayer(p);
+    setOpenOne(true);
+  }
+
 
   return (
     <div>
-      <PlayerPage openOne={openOne} setOpenOne={setOpenOne} />
-        <button
-      className='btn'
-      onClick={handleClickOpen}
-      style={{ marginBottom: '20px'}}
+      <button
+        className='btn'
+        onClick={handleClickOpen}
+        style={{ marginBottom: '20px' }}
       >
         Add Character
       </button>
-      <Dialog maxWidth='md' fullWidth open={open} onClose={handleClose} aria-labelledby="form-dialog-title" style={{zIndex:'1'}}>
-        <DialogContent style={{textAlign: 'center'}}>
-                <AddPlayerDialog addPlayer={addPlayer}  addSavedPlayer={addSavedPlayer} />
+      <Dialog maxWidth='md' fullWidth open={open} onClose={handleClose} aria-labelledby="form-dialog-title" style={{ zIndex: '1' }}>
+        <DialogContent style={{ textAlign: 'center' }}>
+          <AddPlayerDialog addPlayer={addPlayer} addSavedPlayer={addSavedPlayer} />
           <div>
-            <button className="btn"    style={{position:'absolute',right: '25px', top: '25px'}} onClick={handleClose}>cancel</button>
-<h2 style={{fontSize:'20px'}}>Saved Characters</h2>
-{savedPlayers && 
-savedPlayers.map(p => { 
-  return (
-  <div key={p.name}  className="savedPlayersContainer"  >
-  <div
-    className="box" onClick={() => setOpenOne(true)} >
-    <img draggable="false" className='savedPlayersImg' src={p.url} alt="" width='100px' height='auto'/>
-    <br/>
-    {p.name}
-  </div> 
-</div>
-  )}
-)
-}
-</div>
+            <button className="btn" style={{ position: 'absolute', right: '25px', top: '25px' }} onClick={handleClose}>cancel</button>
+            <h2 style={{ fontSize: '20px' }}>Saved Characters</h2>
+            {savedPlayers &&
+              savedPlayers.map(p => {
+                return (
+                  <div key={p.name} className="savedPlayersContainer"  >
+                    <PlayerPage openOne={openOne} setOpenOne={setOpenOne} currentPlayer={currentPlayer} />
+                    <div
+                      className="box" onClick={() => openPlayerPage(p)} >
+                      <img draggable="false" className='savedPlayersImg' src={p.url} alt="" width='100px' height='auto' />
+                      <br />
+                      {p.name}
+                    </div>
+                  </div>
+                )
+              }
+              )
+            }
+          </div>
         </DialogContent>
       </Dialog>
     </div>
