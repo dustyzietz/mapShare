acbb7c63046ac1730186b20beaa25628a9002726
dustyzietz <dustyzietz@gmail.com>
2020-05-13 20:09:28 -1000

    going to make it look better

--- client/src/components/HitPoints.js
+++ client/src/components/HitPoints.js
@@ -1,16 +1,23 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import  HitPointsPlayer  from "./HitPointsPlayer";
 
 const HitPoints = ({ players }) => {
   const [editing, setEditing] = useState(false);
+ const [shuffled, setShuffled] = useState([]);
  
+
+ useEffect(()=> {
+if(shuffled.length == 0) {setShuffled(players);} 
+ },[players]);
+
 
   return (
     <div className='hit-points' style={{ backgroundColor: "black", color: "white" }}>
-     HIT POINTS 
-      {players.map((p, i) =>{
+     HIT POINTS <button onClick={handleInitiative} >Initiative</button>
+      { shuffled &&
+        shuffled.map((p) =>{
         return (
-       <HitPointsPlayer key={i} p={p} editing={editing} players={players} />
+       <HitPointsPlayer key={p._id} p={p} editing={editing} players={players} />
       )
       }
       )}
