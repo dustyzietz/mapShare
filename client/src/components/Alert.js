766b1567556072a61384d9b48229f219a5bd606a
dustyzietz <dustyzietz@gmail.com>
2020-08-14 17:17:30 -1000

    fixed conflict

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 const Alert = ({ alerts }) =>
   alerts !== null &&
   alerts.length > 0 && 
-<<<<<<< HEAD
   alerts.map(alert => (
   <div class="bs-component" style={{position:'fixed', right:'0', top:'0', maxWidth:"500px"}}>
               <div class={`alert alert-dismissible alert-${alert.alertType}`}>
@@ -15,10 +14,6 @@ const Alert = ({ alerts }) =>
   ))
 {/*  <div style={{position:'fixed', right:'0', top:'0', maxWidth:"500px"}} >
   { alerts.map(alert => (
-=======
-  <div style={{position:'fixed', right:'20px', top:'20px',maxWidth:"500px"}} >
- { alerts.map(alert => (
->>>>>>> 85f1d5bb416cc6f5c4af5548b885000c59e81491
     <div key={alert.id} className={`alert alert-${alert.alertType}`}>
       {alert.msg && alert.msg}
     </div>
